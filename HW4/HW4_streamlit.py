63b109140f7542a0e2fb220f8fe8b4a4780690c6
peteruede <pytar01@gmail.com>
2023-10-10 08:17:08 -0400

    ver 1.3

--- HW4/HW4_streamlit.py
+++ HW4/HW4_streamlit.py
@@ -24,7 +24,7 @@ file = r'Loan_Modelling.csv'
 df = pd.read_csv(file, index_col="ID")
 
 # Set Streamlit app title
-st.title("Loan Acceptance Predictor")
+st.title("Enhancing AllLife Bank's Personal Loan Marketing Strategy 🏦")
 
 # AllLife Bank has a growing customer base. Majority of these customers are liability customers (depositors) with varying size of deposits. 
 # The number of customers who are also borrowers (asset customers) is quite small, and the bank is interested in expanding this base rapidly 
@@ -34,17 +34,18 @@ st.title("Loan Acceptance Predictor")
 # This has encouraged the retail marketing department to devise campaigns with better target marketing to increase the success ratio with a minimal budget.
 
 # Add an expander
-expand = st.expander("**Background & Context**")
-expand.write(
+with st.expander("**Background & Context**"):
+    st.write(
     """
-         AllLife Bank aims to grow its customer base, focusing on increasing the number of borrowers (asset customers) while retaining 
-         depositors (liability customers). Last year's campaign for liability customers had a conversion rate of over 9%, inspiring 
-         the retail marketing department to create more efficient, targeted campaigns with a minimal budget to boost this ratio further."""
-)
+    AllLife Bank aims to grow its customer base, focusing on increasing the number of borrowers (asset customers) while retaining 
+    depositors (liability customers). Last year's campaign for liability customers had a conversion rate of over 9%, inspiring 
+    the retail marketing department to create more efficient, targeted campaigns with a minimal budget to boost this ratio further.
+    """
+    )
 
-# Add an expander
-expand2 = st.expander("**Data Dictionary**")
-expand2.write(
+
+with st.expander("**Data Dictionary**"):
+    st.write(
     """
          * `ID`: Unique Customer Identification Number
          * `Age`: Customer’s age in years
@@ -59,8 +60,10 @@ expand2.write(
          * `Securities_Account`: Does the customer have securities account with the bank?
          * `CD_Account`: Does the customer have a certificate of deposit (CD) account with the bank?
          * `Online`: Do customers use internet banking facilities?
-         * `CreditCard`: Does the customer use a credit card issued by Universal Bank?"""
-)
+         * `CreditCard`: Does the customer use a credit card issued by Universal Bank?
+         """
+         )
+
 
 # Designing the Visuals on the App
 # --------------------------------
@@ -68,109 +71,145 @@ expand2.write(
 # Partitioning the Web App to accommodate the Visualization of the Dataset and ML algorithm
 st.sidebar.write("### This Application is divided into two sections")
 
-main_opt = st.sidebar.radio('What do you want to do: ', ["Data Visualization", "Run Machine Learning Algorithms"])
+main_opt = st.sidebar.radio('What do you want to do: ', 
+                            ["Data Visualization", "Run Machine Learning Algorithms"])
 
 if(main_opt == "Data Visualization"):
+    
+    
+
     # Create layout columns
-col1, col2 = st.columns([1, 3])
+    col1, col2 = st.columns([1, 3])
+    
+    # Display unique value counts for each column
+    col1.subheader("Value Count", divider="blue")
+    col1.write(f"Dataset shape: {df.shape}")
+    unique_counts = df.nunique().sort_values(ascending=False)
+    col1.write(unique_counts)
 
-# Display unique value counts for each column
-col1.subheader("Value Count", divider="blue")
-col1.write(f"Dataset shape: {df.shape}")
-unique_counts = df.nunique().sort_values(ascending=False)
-col1.write(unique_counts)
+    # Convert selected columns to categorical variables
+    cat_columns = [
+        "Family",
+        "Education",
+        "Personal_Loan",
+        "Securities_Account",
+        "CD_Account",
+        "Online",
+        "CreditCard"
+        ]
+    df[cat_columns] = df[cat_columns].astype("category")
+    
+    # Summary statistics
+    col2.subheader("Summary Statistics", divider="red")
+    col2.write(df.describe().T)
 
-# Convert selected columns to categorical variables
-cat_columns = [
-    "Family",
-    "Education",
-    "Personal_Loan",
-    "Securities_Account",
-    "CD_Account",
-    "Online",
-    "CreditCard",
-]
-df[cat_columns] = df[cat_columns].astype("category")
+    # Handle negative values in 'Experience'
+    neg_experience_count = (df["Experience"] < 0).sum()
+    col2.write(
+        f"{neg_experience_count} records have negative values for years of experience."
+        )
+    df["Experience"] = df["Experience"].clip(lower=0)
 
-# Display descriptive statistics
-col2.subheader("Summary Statistics", divider="red")
-col2.write(df.describe().T)
+    # Impute missing values in 'Experience' based on median experience for each age group
+    df["Experience"] = df.groupby("Age")["Experience"].transform(
+        lambda x: x.fillna(x.median())
+        )
 
-# Handle negative values in 'Experience'
-neg_experience_count = (df["Experience"] < 0).sum()
-col2.write(
-    f"{neg_experience_count} records have negative values for years of experience."
-)
-df["Experience"] = df["Experience"].clip(lower=0)
 
-# Impute missing values in 'Experience' based on median experience for each age group
-df["Experience"] = df.groupby("Age")["Experience"].transform(
-    lambda x: x.fillna(x.median())
-)
+    # Button that allows the user to see the entire table
+    check_data = st.checkbox('Show the Dataset')
+    if check_data:
+        st.dataframe(df)
 
-# Plot correlation heatmap
-st.write("#### Correlation Heatmap")
-corr_heatmap = sns.heatmap(
-    data=df.corr(numeric_only=True), linewidths=0.5, annot=True, fmt=".2f"
-)
-st.pyplot()
+    st.write("### Have fun with data exploration!")
 
-st.write("## Have fun with data exploration!")
+    # Create tabs for different visualizations
+    sec1, sec2, sec3, sec4, sec5 = st.tabs(["Variable Distribution", 
+                                            "Boxplot", 
+                                            "Pair Plot", 
+                                            "Bar Plot", 
+                                            "Altair Interactive Plot"
+                                            ])
 
-# Create tabs for different visualizations
-sec1, sec2, sec3, sec4 = st.tabs(["Distribution", "Boxplot", "Pair Plot", "Bar Plot"])
+    # Tab 1: Distribution Plot
+    numeric_columns = df.select_dtypes(include=np.number).columns.tolist()
+    non_numeric_columns = df.select_dtypes(exclude=np.number).columns.tolist()
+    selected = sec1.radio("Select the feature you want to display", numeric_columns, horizontal= True, key= "rad1")
+    fig = sns.histplot(data=df, x=selected, hue="Personal_Loan")
+    sec1.pyplot()
 
-# Tab 1: Distribution Plot
-sec1.write("### Distribution Plot")
-numeric_columns = df.select_dtypes(include=np.number).columns.tolist()
-selected = sec1.selectbox(
-    "Which features are you interested in?",
-    [d for d in numeric_columns if d != "Personal_Loan"],
-    key="se1",
-)
-fig = sns.histplot(data=df, x=selected, hue="Personal_Loan", element="step")
-sec1.pyplot()
+    # Tab 2: Box Plot
+    selected2 = sec2.radio("Select the feature you want to display", numeric_columns, horizontal= True, key= "rad2")
 
-# Tab 2: Box Plot
-sec2.write("### Box Plot")
-selected2 = sec2.selectbox(
-    "Which features are you interested in?", numeric_columns, key="se2"
-)
-fig = sns.boxplot(data=df, x=selected2, orient="h")
-sec2.pyplot()
+    fig = sns.boxplot(data=df, x=selected2, orient="h")
+    sec2.pyplot()
 
-# Tab 3: Pair Plot
-sec3.write("### Pair Plot")
-selected3 = sec3.multiselect(
-    "Which features are you interested in?",
-    [d for d in numeric_columns if d != "Personal_Loan"],
-    ["Age", "Income", "Mortgage"],
-    key="se3",
-)
-sns.pairplot(
-    df[["Personal_Loan"] + selected3],
-    hue="Personal_Loan",
-    palette=["blue", "green"],
-    markers=["o", "s"],
-)
-sec3.pyplot()
+    # Tab 3: Pair Plot
+    selected3 = sec3.multiselect(
+        "Which features are you interested in?",
+        [d for d in numeric_columns if d != "Personal_Loan"],
+        ["Age", "Income", "Mortgage"],
+        key="se3"
+        )
+    # Incase the user makes a mistake by deleting the columns by mistake
+    if (len(selected3) == 0):
+        st.write("You cannot leave the field empty, Please select one or more columns!")
+    else:
+        sns.pairplot(
+        df[["Personal_Loan"] + selected3],
+        hue="Personal_Loan",
+        palette=["blue", "green"],
+        markers=["o", "s"]
+        )
+        sec3.pyplot()
 
-# Tab 4: Bar Plot
-sec4.write("### Bar Plot")
-non_numeric_columns = df.select_dtypes(exclude=np.number).columns.tolist()
-selected4 = sec4.selectbox(
-    "Which features are you interested in?", non_numeric_columns, key="se4"
-)
-fig, ax = plt.subplots()
-sns.countplot(data=df, x=selected4, ax=ax)
-total = len(df[selected4])
-for p in ax.patches:
-    height = p.get_height()
-    ax.text(
-        p.get_x() + p.get_width() / 2.0,
-        height + 0.5,
-        f"{100 * height / total:.2f}%",
-        ha="center",
-        size=10,
-    )
-sec4.pyplot()
+    # Tab 4: Bar Plot
+    
+    selected4 = sec4.selectbox(
+        "Which feature are you interested in?", non_numeric_columns, key="se4"
+        )
+    fig, ax = plt.subplots()
+    sns.countplot(data=df, x=selected4, ax=ax)
+    total = len(df[selected4])
+    for p in ax.patches:
+        height = p.get_height()
+        ax.text(
+            p.get_x() + p.get_width() / 2.0,
+            height + 0.5,
+            f"{100 * height / total:.2f}%",
+            ha="center",
+            size=10
+            )
+    sec4.pyplot()
+
+    # Tab 5: Altair Plot
+    opt1, opt2, opt3 = st.columns(3)
+
+    x_sb = opt1.selectbox('x axis: ', numeric_columns)
+    y_sb = opt2.selectbox('y axis: ', numeric_columns)
+    color = opt3.selectbox('hue: ', non_numeric_columns)
+
+    chart = alt.Chart(df).mark_point().encode(
+        alt.X(x_sb, title= f'{x_sb}'),
+        alt.Y(y_sb, title=f'{y_sb}'), 
+        color=alt.Color(color)).properties(
+            width=700,
+            height=550
+            ).interactive()
+    
+    sec5.altair_chart(chart)
+
+    
+
+    # Plot correlation heatmap
+    st.write("#### Correlation Heatmap")
+    corr_heatmap = sns.heatmap(
+        data=df.corr(numeric_only=True), 
+        linewidths=0.5, 
+        annot=True, 
+        fmt=".2f"
+        )
+    st.pyplot()
+
+else:
+    st.write("# Coming Soon! 😊")
